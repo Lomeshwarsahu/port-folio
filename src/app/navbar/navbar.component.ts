68c81b8ac3e9e314f04fe5ee7743990206ef3851
Lomeshwarsahu <lomeshwarsahu1996@gmail.com>
2025-03-21 17:25:43 -0700

    WIP: Before Angular update

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  isDarkMode = false;
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    document.body.classList.toggle('bg-dark', this.isDarkMode);
+    document.body.classList.toggle('text-light', this.isDarkMode);
+    localStorage.setItem('theme', this.isDarkMode ? 'dark' : 'light');
+  }
+
+  ngOnInit() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+      this.isDarkMode = savedTheme === 'dark';
+      document.body.classList.toggle('bg-dark', this.isDarkMode);
+      document.body.classList.toggle('text-light', this.isDarkMode);
+    }
+  }
+}
